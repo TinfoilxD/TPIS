0b7db31edb5d20a09d09f98e74dd94c425541a5a
TinfoilxD <TinfoilxD@gmail.com>
2017-02-25 16:47:55 -0600

    roles defined

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem 'jbuilder', '~> 2.5'
 # Devise for Authentication
 gem 'devise'
 # CanCanCan for Authorization
-#gem 'cancancan'
+gem 'cancancan'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new
+user.email = 'tin@sdc.com'
+user.password = 'GSupremeBunnyOverlordCL'
+user.password_confirmation = 'GSupremeBunnyOverlordCL'
+user.role = 'admin'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'minion@sdc.com'
+user.password = 'minion1'
+user.password_confirmation = 'minion1'
+user.role = 'candidate'
+user.skip_confirmation!
+user.save!
