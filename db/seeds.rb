233f4b50e99ebd43b6961734b2a3e886b331a247
Joshua Joseph <jjoselife@gmail.com>
2017-03-23 20:37:05 +0000

    recommit

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,11 +69,171 @@ user.role = 'candidate'
 user.skip_confirmation!
 user.save!
 
+#-----------------System Admin accounts (Josh)-------------------------
+user = User.new
+user.email = 'freeman.twsami@therapeuticprofessionals.com'
+user.password = 'hkhihhhohi3643#'
+user.password_confirmation = 'hkhihhhohi3643#'
+user.role = 'system_admin'
+user.skip_confirmation!
+user.save!
+#------------------End creating System Admin accounts-----------------
+
+#-----------------TCF Admin accounts (Josh)-------------------------
+user = User.new
+user.email = 'cherri.hudgins@therapeuticprofessionals.com'
+user.password = 'neverstop86'
+user.password_confirmation = 'neverstop86'
+user.role = 'tcf_admin'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'jessica.jones@therapeuticprofessionals.com'
+user.password = 'neverstop86'
+user.password_confirmation = 'neverstop86'
+user.role = 'tcf_admin'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'rhema.yale@therapeuticprofessionals.com'
+user.password = 'fireburd34#'
+user.password_confirmation = 'fireburd34#'
+user.role = 'tcf_admin'
+user.skip_confirmation!
+user.save!
+#------------------End creating TCF Admin accounts-----------------
+
+#------------------TCF accounts (Josh)-----------------------------
+user = User.new
+user.email = 'ray.smith@therapeuticprofessionals.com'
+user.password = 'swingryt45'
+user.password_confirmation = 'swingryt45'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'luke.cage@slb.com'
+user.password = 'superstrong783455'
+user.password_confirmation = 'superstrong783455'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'yumi.yui@therapeuticprofessionals.com'
+user.password = 'wogejwo455'
+user.password_confirmation = 'wogejwo455'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'ben.john@tlc.com'
+user.password = 'gjsogji3533'
+user.password_confirmation = 'gjsogji3533'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'mike.jones@therapeuticprofessionals.com'
+user.password = 'whomikejones325325523'
+user.password_confirmation = 'whomikejones325325523'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'larry.burd@therapeuticprofessionals.com'
+user.password = 'ojsggsoig4344'
+user.password_confirmation = 'ojsggsoig4344'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'emanda.burns@therapeuticprofessionals.com'
+user.password = 'sjowjgewg5232'
+user.password_confirmation = 'sjowjgewg5232'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'tiffany.royal@therapeuticprofessionals.com'
+user.password = 'ewoijgioj432399'
+user.password_confirmation = 'ewoijgioj432399'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'haley.ball@therapeuticprofessionals.com'
+user.password = 'bswojgjoi325525'
+user.password_confirmation = 'bswojgjoi325525'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'quenn.latifah@therapeuticprofessionals.com'
+user.password = 'ttwhwosijgixgg65'
+user.password_confirmation = 'ttwhwosijgixgg65'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'tim.holloway@therapeuticprofessionals.com'
+user.password = 'ojiwhxklwoi352'
+user.password_confirmation = 'ojiwhxklwoi352'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'emely.wang@bltpa.com'
+user.password = 'wgoiwojg34535'
+user.password_confirmation = 'wgoiwojg34535'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'terry.cruz@cruzrb.com'
+user.password = 'xgiohisjgja43532'
+user.password_confirmation = 'xgiohisjgja43532'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'samantha.curry@therapeuticprofessionals.com'
+user.password = 'gsiopqjogj83838'
+user.password_confirmation = 'gsiopqjogj83838'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+
+user = User.new
+user.email = 'damon.green@therapeuticprofessionals.com'
+user.password = 'nieiwngoinw32425'
+user.password_confirmation = 'nieiwngoinw32425'
+user.role = 'tcf'
+user.skip_confirmation!
+user.save!
+#------------------End creating TCF accounts-----------------
+
 AllignmentType.create(allignment_type: 'employee')
 AllignmentType.create(allignment_type: 'contractor')
 AllignmentType.create(allignment_type: 'training_center')
 
 Candidate.create(email: 'tin@sdc.com', first_name: 'Hamburger', last_name: 'Steak')
+Candidate.create(email: 'minion5@sdc.com', first_name: 'minion', last_name: '5')
+Candidate.create(email: '', first_name: 'Mike', last_name: 'Jones')
 Candidate.create(email: 'minion5@sdc.com', first_name: 'minion', last_name: '5')
 
 CourseType.create([{course_type: 'BLS'},{course_type: 'HS'}, {course_type: 'ACLS'}, {course_type: 'PALS'}])
