96a53b007786b88c7b521793d265bb063e2c1244
TinfoilxD <TinfoilxD@gmail.com>
2017-03-23 15:39:38 -0500

    changed email production settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   #mailer setup for development. Production should have an actual host.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.smtp_settings = {
+      user_name:      'sleightdataconsuling@gmail.com',
+      password:       'project4TPInstructors',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 end
