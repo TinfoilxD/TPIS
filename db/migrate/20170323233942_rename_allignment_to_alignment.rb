af280000be70dd45ce61e8698b15e887d8fd47d4
etzul <emely.sheng@gmail.com>
2017-03-25 13:30:54 -0500

    created_landing_pages

--- db/migrate/20170323233942_rename_allignment_to_alignment.rb
+++ db/migrate/20170323233942_rename_allignment_to_alignment.rb
@@ -1,5 +1,5 @@
 class RenameAllignmentToAlignment < ActiveRecord::Migration[5.0]
   def change
-    rename_table :allignmentTypes, :alignment_types
+    rename_table :AlignmentType, :alignment_types
   end
 end
