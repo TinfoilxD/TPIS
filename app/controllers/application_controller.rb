2b8a5958d483ea98bca04271cc991dcb062ab1cc
Daniel Villarreal Jr <dan.vill.jr3@gmail.com>
2017-04-02 11:35:59 -0500

    fix some navigation related to changes in applications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     pathing_path
   end
+
+  def set_route_info
+    session[:return_to] = request.url
+
+    prev_route = Rails.application.routes.recognize_path(URI(session[:return_to]).path)
+
+
+    session[:previous_controller] = prev_route[:action]
+    session[:previous_action] = prev_route[:action]
+  end
+
 end
