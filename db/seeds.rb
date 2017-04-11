31b2401609d330fe9084cf515d73ef186c75005b
etzul <emely.sheng@gmail.com>
2017-04-11 04:45:49 -0500

    master

--- db/seeds.rb
+++ db/seeds.rb
@@ -645,10 +645,10 @@ user.save!
 #========== End User Accounts ==========#
 
 # original Alignment types
-AlignmentType.create(alignment_type: 'employee')
-AlignmentType.create(alignment_type: 'independent contractor')
-AlignmentType.create(alignment_type: 'training_center')
-AlignmentType.create(alignment_type: 'contract employee')
+AlignmentType.create(alignment_type: 'Employee')
+AlignmentType.create(alignment_type: 'Independent Contractor')
+AlignmentType.create(alignment_type: 'Training Center')
+AlignmentType.create(alignment_type: 'Contract Employee')
 
 
 
@@ -733,6 +733,7 @@ Candidate.create(email: '', first_name: 'Mike', last_name: 'Jones', phone: '7777
 
 
 
+
 CourseType.create([{course_type: 'BLS'},{course_type: 'HS'}, {course_type: 'ACLS'}, {course_type: 'PALS'}])
 
 Course.create(course_type_id: CourseType.where(course_type: 'BLS').first.id,
@@ -763,6 +764,7 @@ Course.create(course_type_id: CourseType.where(course_type: 'BLS').first.id,
               state: 'TX',
               country: 'United States',
               course_zipcode: '77022')
+
 t1=TrainingCenter.create(training_center_name: 'Therapeutical Professionals',
                       primary_contact: 'Mary Turner',
                       phone: '281-702-1243',
@@ -773,7 +775,8 @@ t1=TrainingCenter.create(training_center_name: 'Therapeutical Professionals',
 # Faculties
 #1-2
 FacultyType.create(job_title: 'TCF Admin')
-FacultyType.create(job_title: 'Cheri')
+FacultyType.create(job_title: 'Registered Nurse')
+FacultyType.create(job_title: 'Doctor')
 
 #1-2
 f1 = Faculty.create(first_name: 'Ashley' , last_name: 'Micheal', faculty_type_id: FacultyType.where(job_title: 'placeholder faculty type 1').first, email: 'ashmchl@hotmail.com', phone: '713-455-2309' ,  office_address: '3354 Stafford Rd.' ,  state: 'Texas' , zip_code: '77006' , country: 'United States')
@@ -886,3 +889,60 @@ user.password_confirmation = 'slammer3'
 user.role = 'candidate'
 user.skip_confirmation!
 user.save!
+
+# States
+
+State = State.create!([
+                           { state_name: 'Alaska', state_code: 'AK' },
+                           { state_name: 'Alabama', state_code: 'AL' },
+                           { state_name: 'Arkansas', state_code: 'AR' },
+                           { state_name: 'Arizona', state_code: 'AZ' },
+                           { state_name: 'California', state_code: 'CA' },
+                           { state_name: 'Colorado', state_code: 'CO' },
+                           { state_name: 'Connecticut', state_code: 'CT' },
+                           { state_name: 'District of Columbia', state_code: 'DC' },
+                           { state_name: 'Delaware', state_code: 'DE' },
+                           { state_name: 'Florida', state_code: 'FL' },
+                           { state_name: 'Georgia', state_code: 'GA' },
+                           { state_name: 'Hawaii', state_code: 'HI' },
+                           { state_name: 'Iowa', state_code: 'IA' },
+                           { state_name: 'Idaho', state_code: 'ID' },
+                           { state_name: 'Illinois', state_code: 'IL' },
+                           { state_name: 'Indiana', state_code: 'IN' },
+                           { state_name: 'Kansas', state_code: 'KS' },
+                           { state_name: 'Kentucky', state_code: 'KY' },
+                           { state_name: 'Louisiana', state_code: 'LA' },
+                           { state_name: 'Massachusetts', state_code: 'MA' },
+                           { state_name: 'Maryland', state_code: 'MD' },
+                           { state_name: 'Maine', state_code: 'ME' },
+                           { state_name: 'Michigan', state_code: 'MI' },
+                           { state_name: 'Minnesota', state_code: 'MN' },
+                           { state_name: 'Missouri', state_code: 'MO' },
+                           { state_name: 'Mississippi', state_code: 'MS' },
+                           { state_name: 'Montana', state_code: 'MT' },
+                           { state_name: 'North Carolina', state_code: 'NC' },
+                           { state_name: 'North Dakota', state_code: 'ND' },
+                           { state_name: 'Nebraska', state_code: 'NE' },
+                           { state_name: 'New Hampshire', state_code: 'NH' },
+                           { state_name: 'New Jersey', state_code: 'NJ' },
+                           { state_name: 'New Mexico', state_code: 'NM' },
+                           { state_name: 'Nevada', state_code: 'NV' },
+                           { state_name: 'New York', state_code: 'NY' },
+                           { state_name: 'Ohio', state_code: 'OH' },
+                           { state_name: 'Oklahoma', state_code: 'OK' },
+                           { state_name: 'Oregon', state_code: 'OR' },
+                           { state_name: 'Pennsylvania', state_code: 'PA' },
+                           { state_name: 'Puerto Rico', state_code: 'PR' },
+                           { state_name: 'Rhode Island', state_code: 'RI' },
+                           { state_name: 'South Carolina', state_code: 'SC' },
+                           { state_name: 'South Dakota', state_code: 'SD' },
+                           { state_name: 'Tennessee', state_code: 'TN' },
+                           { state_name: 'Texas', state_code: 'TX' },
+                           { state_name: 'Utah', state_code: 'UT' },
+                           { state_name: 'Virginia', state_code: 'VA' },
+                           { state_name: 'Vermont', state_code: 'VT' },
+                           { state_name: 'Washington', state_code: 'WA' },
+                           { state_name: 'Wisconsin', state_code: 'WI' },
+                           { state_name: 'West Virginia', state_code: 'WV' },
+                           { state_name: 'Wyoming', state_code: 'WY' }
+                       ])
