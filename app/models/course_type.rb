d3d2969877eef070bc4cae71b936f3e6dbb80420
Daniel Villarreal Jr <dan.vill.jr3@gmail.com>
2017-04-11 20:38:22 -0500

    New method in course_type model

--- app/models/course_type.rb
+++ app/models/course_type.rb
@@ -1,3 +1,11 @@
 class CourseType < ApplicationRecord
   has_many :courses
+  has_many :application_forms
+
+  validates_presence_of :course_type
+
+  def course_type_identifier(course_type_id)
+    course_type = CourseType.find(course_type_id).course_type
+    "#{course_type}"
+  end
 end
