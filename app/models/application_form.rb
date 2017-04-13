295917fbb0ba7db4574251734a4e87ab8d489fd3
Daniel Villarreal Jr <dan.vill.jr3@gmail.com>
2017-04-13 16:50:28 -0500

    Lots of changes regarding the handling of App form Data

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -2,7 +2,14 @@ class ApplicationForm < ApplicationRecord
   belongs_to :training_center
   belongs_to :course_type
   belongs_to :candidate
+  belongs_to :state
 
 
   accepts_nested_attributes_for :course_type
+
+  def get_course_address
+    course_data = "Starts on #{course_start_date.strftime("%m/%d/%y")} in #{course_city}, #{state&.state_code}"
+    return course_data
+
+  end
 end
