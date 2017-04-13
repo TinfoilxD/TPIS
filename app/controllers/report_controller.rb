19e88a4c305f125e3edeeab8db1f5d67ea627a05
ajdalal <avishka9119@gmail.com>
2017-04-13 17:49:00 -0500

    report4

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -65,5 +65,19 @@ class ReportController < ApplicationController
   def candidates_by_course
     @candidates = Candidate.all
     @course_types = CourseType.all
-   end
+  end
+
+  def alignment_employees
+    aligned_type = "'Employee'"
+    sql_statement = "SELECT candidates.first_name as afn, "\
+                    "candidates.last_name as aln, "\
+                    "alignment_types.alignment_type as aat "\
+                    "FROM candidates "\
+                    "INNER JOIN alignment_types ON candidates.alignment_type_id =  alignment_types.id "\
+                    "WHERE alignment_types.alignment_type = #{aligned_type}"
+    @employees = ActiveRecord::Base.connection.execute(sql_statement)
+    @candidates = Candidate.joins(:alignment_type).where("alignment_types.alignment_type = 'Employee'").
+        pluck(:first_name, :last_name, :alignment_type)
+
+  end
 end
